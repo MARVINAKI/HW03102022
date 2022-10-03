9756b5cd7ccc8775b83d73b63e45d91384ecb9d6
MARVINAKI <kosakbig@gmial.com>
2022-10-03 21:00:04 +0500

    HW03102022 Part 2 (Ex.1-Ex.7)

--- src/Main.java
+++ src/Main.java
@@ -21,5 +21,32 @@ public class Main {
         System.out.println();
 
 
+    //Exercise 5
+        for (int c=1904;c<=2096;c+=4) {
+            if (c%100==0&&c%400!=0) {
+                c+=4;
+            }
+            System.out.println(c+" год является високосным");
+        }
+        System.out.println();
+
+
+    //Exercise 6
+        for (int c=7;c<=98;c+=7) System.out.println(c);
+        System.out.println();
+
+
+    //Exercise 7
+        for (int c=1;c<=512;c*=2) System.out.println(c);
+        System.out.println();
+
+
+    //Exercise 8
+
+
+
+
+
+
     }
 }
