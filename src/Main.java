07bfb2e2c2ac16236232325cd0ceb7b787c3048f
MARVINAKI <kosakbig@gmial.com>
2022-10-03 20:43:58 +0500

    HW03102022 Part 1 (Ex.1-Ex.4)

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+
+    //Exercise 1
+        for (int c=1;c<=10;c++) System.out.println(c);
+        System.out.println();
+
+
+    //Exercise 2
+        for (int c=10;c>=1;c--) System.out.println(c);
+        System.out.println();
+
+
+    //Exercise 3
+        for (int c=0;c<=17;c+=2) System.out.println(c);
+        System.out.println();
+
+
+    //Exercise 4
+        for (int c=10;c>=-10;c--) System.out.println(c);
+        System.out.println();
+
+
+    }
+}
